c7319dc441d565e56b9be66dc85023c7dc040026
KomilPulatov <k.pulatov2006@gmail.com>
2026-08-21 17:14:19 +0500

    wip

--- order-service/routes/api.php
+++ order-service/routes/api.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\Api\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/orders', [OrderController::class, 'index']);
-    Route::post('/orders', [OrderController::class, 'store']);
-    Route::get('/orders/{order}', [OrderController::class, 'show']);
-    Route::post('/orders/{order}/cancel', [OrderController::class, 'cancel']);
-});
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders/{order}', [OrderController::class, 'show']);
+Route::post('/orders/{order}/cancel', [OrderController::class, 'cancel']);
--- product-service/routes/api.php
+++ product-service/routes/api.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::patch('/products/{product}', [ProductController::class, 'update']);
-    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
-});
+Route::post('/products', [ProductController::class, 'store']);
+Route::patch('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
